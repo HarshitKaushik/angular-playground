f122fe0d9ebbe660c298de19f2801660c91e65a7
Harshit Kaushik <harshit@kwench.in>
2019-03-09 22:17:01 +0530

    shopping app basic components

--- udemy-course-app/src/app/app.module.ts
+++ udemy-course-app/src/app/app.module.ts
@@ -2,12 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from 'src/app/server/server.component';
+import { HeaderComponent } from 'src/app/header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { EditShoppingComponent } from './shopping-list/edit-shopping/edit-shopping.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    EditShoppingComponent
   ],
   imports: [
     BrowserModule
---
+++ udemy-course-app/src/app/shopping-list/edit-shopping/edit-shopping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-shopping',
+  templateUrl: './edit-shopping.component.html',
+  styleUrls: ['./edit-shopping.component.scss']
+})
+export class EditShoppingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
